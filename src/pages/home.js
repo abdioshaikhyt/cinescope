0e4eb992cd3b14d1137cd5841780a2aa3f92f528
abdioshaikhyt <abdioshaikh61@gmail.com>
2026-06-23 04:47:54 +0100

    feat: complete renderCards with full card structure, genre lookup, and DOM attachment

--- src/pages/home.js
+++ src/pages/home.js
@@ -29,12 +29,23 @@ function renderCards(movies, container, genreMap) {
         const metadataWrapper = document.createElement('div');
         metadataWrapper.className = 'movie-metadata'
         const metadataHFour = document.createElement('h4');
+        metadataHFour.textContent = movie.title;
         const cardMetaWrapper = document.createElement('div');
         cardMetaWrapper.className = 'card-meta';
         const ratingTag = document.createElement('p');
+        ratingTag.textContent = movie.vote_average.toFixed(1);
         const genreTag = document.createElement('p');
+        genreTag.textContent = genreMap[movie.genre_ids[0]];
         const yearTag = document.createElement('p');
-
-    });
+        yearTag.textContent = movie.release_date.slice(0,4);
+        wrapper.appendChild(wrapperImg);
+        wrapper.appendChild(metadataWrapper);
+        metadataWrapper.appendChild(metadataHFour);
+        metadataWrapper.appendChild(cardMetaWrapper);
+        cardMetaWrapper.appendChild(ratingTag);
+        cardMetaWrapper.appendChild(genreTag);
+        cardMetaWrapper.appendChild(yearTag);
+        container.appendChild(wrapper);
+    }); 
 
 }
